0c31dcc77883674620bb1273ccc0f18ad8634f19
Sergunkit <sergunkit@mail.ru>
2022-12-23 21:49:25 +0300

    add dir games

---
+++ bin/games/brain-even.js
@@ -0,0 +1,20 @@
+// Реализация игры на определение четности чисел
+import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
+
+const isEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const number = Math.ceil(Math.random() * 10);
+  const even = number % 2 === 0;
+  console.log(`Question: ${number}`);
+  const answer = (readlineSync.question('Your answer: '));
+  if ((even && answer === 'yes') || (!even && answer === 'no')) {
+    console.log('Correct!');
+    return 1; // Возвращает '1' для счета корректных ответов в функции game
+  }
+  console.log(`${answer} is wrong answer ;(. Correct answer was '${even ? 'yes' : 'no'}'.`);
+  console.log(`Let's try again, ${name}!`);
+  return 0; // При неверном ответе возвращает '0'
+};
+
+export default isEven; // Экспорт для brain-games
---
+++ bin/games/brain-progression.js
@@ -0,0 +1,28 @@
+// Реализация игры на последовательности
+import readlineSync from 'readline-sync';
+import { name } from '../src/cli.js';
+
+const progression = () => {
+  console.log('What number is missing in the progression?');
+  let start = Math.ceil(Math.random() * 10); // Начало последовательности
+  const step = Math.ceil(Math.random() * 10); // Шаг
+  const long = Math.ceil(Math.random() * 10) + 5; // Длинна последовательности
+  const leftpos = Math.abs(Math.ceil(Math.random() * 10) - long); // индекс пропуска числа
+  const numbers = [];
+  for (let i = 0; i < long; i += 1) { // Формирование последовательности
+    numbers.push(start);
+    start += step;
+  }
+  const correctNum = numbers[leftpos];
+  numbers[leftpos] = '..'; // Присваивание '..' на место пропуска
+  console.log(`Question: ${numbers}`);
+  const answer = Number(readlineSync.question('Your answer: '));
+  if (correctNum === answer) {
+    console.log('Correct!');
+    return 1; // Возврат '1' для подсчета правилных ответов
+  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was ${correctNum}.\nLet's try again, ${name}!`);
+  return 0; // При неправильном ответе возвращается '0'
+};
+
+export default progression; // Экспорт для brain-games
