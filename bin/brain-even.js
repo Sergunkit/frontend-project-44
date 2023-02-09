83c565494a81987e04e40d0c2f800b19963d8a36
Sergunkit <sergunkit@mail.ru>
2023-02-09 19:54:29 +0300

    trying2

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Модуль запускает игру чёт-нечет.
-import isEven from '../src/games/brain-even.js';
+import isEven from '../src/games/even.js';
 import game from '../src/index.js';
 
 game(isEven);
